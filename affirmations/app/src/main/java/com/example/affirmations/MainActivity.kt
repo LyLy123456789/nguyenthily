fa15ce5f94ef7287b14af5c6161466a813851fda
LyLy123456789 <97490213+LyLy123456789@users.noreply.github.com>
2022-02-22 13:24:21 +0700

    Add source

---
+++ affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.affirmations
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.affirmations.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
